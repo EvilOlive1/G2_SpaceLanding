246d54ba364b8154c3e10bb9175e068b0adcea43
Jeremy <jeremylee874@gmail.com>
2024-01-28 21:39:15 -0500

    V1.1.1

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 **G2: Space Landing**
 
 **Jan 28, 2024 - V1.0** - ADDED: Window created from pygame
+
+**Notes**
+
+1 frame/second * 10 px/second = 10px/second
--- SpaceLanding.py
+++ SpaceLanding.py
@@ -1,15 +1,18 @@
 import pygame
+from sys import exit
 
 # Initializing
 pygame.init()
 
 # Creating the window
 screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption("Space Landing")
 
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
+			exit()
 	# Draw all the elements
 	# Update everything
 	pygame.display.update()
